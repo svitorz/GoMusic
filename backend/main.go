e6fc5bbe81d2cab9724a48f41f7c9614651f620b
svitorz <vitor.fabio.castro@gmail.com>
2025-06-13 23:13:13 -0300

    fix: alterando pastas do projeto

---
+++ backend/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/svitorz/GoMusic/router"
+)
+
+func main() {
+	appRoutes := gin.Default()
+
+	router.SetupRouter(appRoutes)
+
+	appRoutes.Run(":8000")
+}
