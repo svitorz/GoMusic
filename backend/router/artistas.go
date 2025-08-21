bce49eb9f407a9595bd6681376746f1bccb8af59
svitorz <vitor.fabio.castro@gmail.com>
2025-08-21 08:31:26 -0300

    feat: hello world via json

--- backend/router/artistas.go
+++ backend/router/artistas.go
@@ -9,10 +9,7 @@ import (
 func getArtistasRoutes(rg *gin.RouterGroup) {
 	artistas := rg.Group("/artistas")
 
-	// TODO: criar funcoes no controller para as rotas
 	artistas.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Olá, sua api de músicas está funcionando")
+		c.JSONP(http.StatusOK, "Olá, sua api de músicas está funcionando")
 	})
-
-	// artistas.GET(":nickname")
 }
