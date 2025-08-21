ce07374f53858615e7867e0cd0149afc4eefd0be
svitorz <vitor.fabio.castro@gmail.com>
2025-08-21 10:38:18 -0300

    feat: run at main

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/svitorz/GoMusic/backend/config"
+	"github.com/svitorz/GoMusic/backend/database"
 	"github.com/svitorz/GoMusic/backend/router"
 )
 
@@ -10,5 +15,13 @@ func main() {
 
 	router.SetupRouter(appRoutes)
 
+	dbconf := config.LoadDatabase()
+
+	db, err := database.ConnectDb(dbconf)
+	if err != nil {
+		log.Fatal("Error to connect to database")
+	}
+
+	fmt.Println(db)
 	appRoutes.Run(":8080")
 }
