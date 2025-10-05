86f724b614c6898fd2e41f14611022d430400e3e
svitorz <vitor.fabio.castro@gmail.com>
2025-10-05 10:50:37 -0300

    feat: criar funcoes para login e registro

--- backend/controllers/users.go
+++ backend/controllers/users.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"log"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/svitorz/GoMusic/backend/auth"
 	"github.com/svitorz/GoMusic/backend/config"
 	"github.com/svitorz/GoMusic/backend/database"
 	"github.com/svitorz/GoMusic/backend/models"
@@ -16,17 +18,77 @@ func GetUsers(c *gin.Context) {
 	db, err := database.ConnectDB(dbconf)
 	if err != nil {
 		log.Print(err)
-		log.Print("Error to connect to database")
+		c.JSON(500, gin.H{"error": "Error to connect to database"})
 	}
 
 	result := db.Find(&users)
-	c.JSON(200, gin.H{"message": "Success!", "users": result})
+	c.JSON(200, gin.H{"users": result})
 }
 func GetUserByID(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "Get user by ID"})
 }
 func CreateUser(c *gin.Context) {
-	c.JSON(201, gin.H{"message": "Create a new user"})
+	dbconf := config.LoadDatabase()
+
+	db, err := database.ConnectDB(dbconf)
+	if err != nil {
+		log.Print(err)
+		log.Print("Error to connect to database")
+	}
+
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = time.Now()
+	user.Role = "customer"
+
+	result := db.Create(&user)
+	if result.Error != nil {
+		c.JSON(500, gin.H{"error": result.Error.Error()})
+		return
+	}
+	log.Printf("User created: %v", user)
+	c.JSON(201, "User created successfully")
+}
+
+func Login(c *gin.Context) {
+	var err error
+
+	user := models.User{}
+
+	dbconf := config.LoadDatabase()
+
+	db, err := database.ConnectDB(dbconf)
+	if err != nil {
+		log.Print(err)
+		log.Print("Error to connect to database")
+		return
+	}
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	result := db.Where("email = ? AND password = ?", user.Email, user.Password).First(&user)
+	if result.Error != nil {
+		c.JSON(401, gin.H{"error": "Invalid email or password"})
+		return
+	}
+
+	log.Printf("User logged in: %v", user)
+	token, err := auth.GenerateToken(user.ID)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Error generating token"})
+		return
+	}
+	c.JSON(200, gin.H{
+		"token": token,
+	})
 }
 func UpdateUser(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "Update user by ID"})
