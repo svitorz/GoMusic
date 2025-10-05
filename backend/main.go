b0c2ff202997e3ce59048868d006a70f4cb94160
svitorz <vitor.fabio.castro@gmail.com>
2025-10-04 23:02:01 -0300

    fix: ajustar main

--- backend/main.go
+++ backend/main.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	"log"
-
-	"github.com/gin-gonic/gin"
-	"github.com/svitorz/GoMusic/backend/config"
-	"github.com/svitorz/GoMusic/backend/database"
 	"github.com/svitorz/GoMusic/backend/router"
 )
 
 func main() {
-	appRoutes := gin.Default()
-
-	router.SetupRouter(appRoutes)
-
-	dbconf := config.LoadDatabase()
-
-	_, err := database.ConnectDB(dbconf)
-	if err != nil {
-		log.Print(err)
-		log.Print("Error to connect to database")
-	}
+	appRoutes := router.SetupRouter()
 
 	appRoutes.Run()
 }
