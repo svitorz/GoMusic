bc3aa9c04ec5c2260f8ee62c7f3252b8fbab5722
svitorz <vitor.fabio.castro@gmail.com>
2025-10-05 10:50:03 -0300

    feat: ajustar verificacaod de token

--- backend/middleware/auth.go
+++ backend/middleware/auth.go
@@ -4,12 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/svitorz/GoMusic/backend/auth"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		token := c.GetHeader("Authorization")
-		if token != "valid-token" {
+		err := auth.TokenValid(c)
+		if err != nil {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 			return
 		}
